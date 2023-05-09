61284ec42c525d067954b1b764ee8737535ea4ec
Johnatan Ortiz <6403@holbertonstudents.com>
2023-05-09 07:07:57 -0700

    solved

--- principal_func.c
+++ principal_func.c
@@ -6,21 +6,13 @@
  *
  */
 
-void f_file(char *file)
+void f_func(char *file)
 {
 	FILE *fpointer;
 
 	fpointer = fopen(file, "r+");
 	if (!fpointer)
 	{
-=======
-void f_func(char *file)
-{
-    FILE * fpointer;
-    fpointer = fopen(file, "r+");
-
-    if (!fpointer)
-    {
 		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", file);
 		exit(EXIT_FAILURE);
 	}
