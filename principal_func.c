37300e51909c45961bf55232db69db9ab453988a
jegomezV <jegomezvalencia@gmail.com>
2023-05-09 11:27:06 -0500

    pointers and vars2

--- principal_func.c
+++ principal_func.c
@@ -13,6 +13,7 @@ void f_func(char *file)
     void (*opcode_func)(stack_t **, unsigned int);
     int line_number = 1;
 	stack_t *head;
+    head = NULL;
 
     fpointer = fopen(file, "r+");
 
@@ -20,5 +21,17 @@ void f_func(char *file)
     {
 		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", file);
 		exit(EXIT_FAILURE);
+	}
+
+    while (getline(&buffer, &read_size, fpointer) != -1)
+	{
+		opcode_func = check_opcodes();
+		if (opcode_func == NULL)
+		{
+			dprintf(STDERR_FILENO, "L%i: unknown instruction %s", line_number, buff);
+			exit(EXIT_FAILURE);
+		}
+		opcode_func(&head, line_number);
+		line_number++;
 	}
 }
