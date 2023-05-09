c4a1fc571f1a86906fefc7e887a4e2125adfae22
Johnatan Ortiz <6403@holbertonstudents.com>
2023-05-09 06:49:48 -0700

    betty style added

--- monty.c
+++ monty.c
@@ -1,11 +1,18 @@
 #include "monty.h"
-
-int main(int argc, char **argv){
-
-    if (argc != 2){
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
-    f_func(argv[1]);
+/**
+ * main - interpreter for Monty ByteCodes files.
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	f_func(argv[1]);
 	return (0);
 }
--- principal_func.c
+++ principal_func.c
@@ -1,12 +1,19 @@
 #include "monty.h"
 
-void f_file(char *file){
-    FILE * fpointer;
-    fpointer = fopen(file, "r+");
-    if (!fpointer)
+/**
+ * f_file - any
+ * @file: name of the file
+ *
+ */
+
+void f_file(char *file)
+{
+	FILE *fpointer;
+
+	fpointer = fopen(file, "r+");
+	if (!fpointer)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't open file %s\n", file);
 		exit(EXIT_FAILURE);
 	}
-
 }
