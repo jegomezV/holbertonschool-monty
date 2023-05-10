dc5f36a8a6871e9925c5815350e41de06726edd5
jegomezV <jegomezvalencia@gmail.com>
2023-05-09 19:59:18 -0500

    reset

--- monty.h
+++ monty.h
@@ -5,65 +5,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
-
-/* GLOBAL VARS */
-
-char *buffer;
-
-/* STRUCTURES */
-
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
- */
-
-typedef struct stack_s
-{
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
-} stack_t;
-
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int);
-} instruction_t;
-
-/* PROTOTYPES */
-
-/* main functions */
-void f_funct(char *file);
-void (*check_opcodes(void))(stack_t **, unsigned int);
-
-/* more functions*/
-void *_calloc(unsigned int nmemb, unsigned int size);
-char **_split(char *str, char *sep);
-int _atoi(char *str, unsigned int line_number);
-void free_stack(stack_t *head);
-
-/* nodes functions */
-void push(stack_t **stack, unsigned int line_number);
-
-/* print functions */
-void pall(stack_t **stack, unsigned int line_number);
 
 #endif
