bb467e396fa6cbd5c5919c044d791d856178af0c
jegomezV <jegomezvalencia@gmail.com>
2023-05-09 19:22:49 -0500

    add extra funcs

--- print_functs.c
+++ print_functs.c
@@ -8,11 +8,10 @@
 void pall(stack_t **stack, unsigned int line_number)
 {
 	(void)line_number;
-	stack_t *current = *stack;
 
-	while (current != NULL)
+	while (*stack)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%i\n", (*stack)->n);
+		stack = &(*stack)->next;
 	}
 }
